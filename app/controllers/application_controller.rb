cce35b16ac2b1a112ca7cbaf67a90e9bd8bb7eb2
Hillary Muller <hillaryaward@gmail.com>
2022-10-04 11:12:20 -0500

    commit tree 6f7fd7f9217fc3ade9ee481457e12e82feb84294

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+private
+def render_not_found
+    render json: { error: "not found" }, status: :not_found
+end
+def unprocessable_entity(invalid)
+    render json: { error: invalid.record.errors }, status: :unprocessable_entity
+end
 end
